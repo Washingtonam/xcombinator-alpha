02080ac39471f463655ddd9e3905f527dbeb7f12
Washington Amedu <washingtonamedu@gmail.com>
2026-05-28 12:16:21 +0100

    feat: add complete modular backend and frontend project structure

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login } = require('../controllers/auth.controller');
 const { protect } = require('../middleware/authMiddleware');
 const { getProfile } = require('../controllers/user.controller');
 
